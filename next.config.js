1c8135b7f3ec680213905a2cc17469f01d042710
welington199222-cmd <welington199222@gmail.com>
2025-10-30 22:23:34 -0300

    Convert next.config.mjs to next.config.js format

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  srcDir: 'src',
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -49,4 +49,4 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
